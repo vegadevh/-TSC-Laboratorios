322bcbabdda92ae8b0ff57bbcde9589ab28941c7
vegadevh <00148816@uca.edu.sv>
2020-03-24 23:17:14 -0600

    Modificado por Criterio: 'La funcion debe recibir 2 parametros', la funcion recibi 3

--- Laboratorio1/main.cpp
+++ Laboratorio1/main.cpp
@@ -4,7 +4,7 @@
 #include "display_tools.h"
 
 int main(void) {
-  
+  /*
   Matrix my_matrix, cof_matrix, adj_matrix, answer_matrix;
 
   zeroes(my_matrix, 3);
@@ -27,4 +27,19 @@ int main(void) {
     showMatrix(answer_matrix);
   }
 
+  */
+  
+  Matrix my_matrix, answer_matrix;
+
+  zeroes(my_matrix, 3);
+
+  my_matrix.at(0).at(0) = 2; my_matrix.at(0).at(1) = 2; my_matrix.at(0).at(2) = 3;
+  my_matrix.at(1).at(0) = 4; my_matrix.at(1).at(1) = 5; my_matrix.at(1).at(2) = 6;
+  my_matrix.at(2).at(0) = 7; my_matrix.at(2).at(1) = 8; my_matrix.at(2).at(2) = 9;
+
+  inversa(my_matrix, answer_matrix);
+
+  showMatrix(answer_matrix);
+  
+  return 0;
 }
--- Laboratorio1/math_tools.h
+++ Laboratorio1/math_tools.h
@@ -10,65 +10,92 @@ using namespace std;
 typedef vector<float> Vector;
 typedef vector<Vector> Matrix;
 
-void zeroes(Matrix &M,int n){
-    for(int i=0;i<n;i++){
-        vector<float> row(n,0.0);
+void zeroes(Matrix &M, int n) {
+    for (int i = 0; i < n; i++) {
+        vector<float> row(n, 0.0);
         M.push_back(row);
     }
 }
 
-void copyMatrix(Matrix A, Matrix &copy){
-    zeroes(copy,A.size());
-    for(int i=0;i<A.size();i++)
-        for(int j=0;j<A.at(0).size();j++)
+void copyMatrix(Matrix A, Matrix &copy) {
+    zeroes(copy, A.size());
+    for (int i = 0; i < A.size(); i++)
+        for (int j = 0; j < A.at(0).size(); j++)
             copy.at(i).at(j) = A.at(i).at(j);
 }
 
-void getMinor(Matrix &M,int i, int j){
-    M.erase(M.begin()+i); 
-    for(int i=0;i<M.size();i++)
-        M.at(i).erase(M.at(i).begin()+j);
+void getMinor(Matrix &M, int i, int j) {
+    M.erase(M.begin() + i);
+    for (int i = 0; i < M.size(); i++)
+        M.at(i).erase(M.at(i).begin() + j);
 }
 
-float determinant(Matrix M){
-    if(M.size() == 1) return M.at(0).at(0);
-    else{
-        float det=0.0;
-        for(int i=0;i<M.at(0).size();i++){
+float determinant(Matrix M) {
+    if (M.size() == 1) return M.at(0).at(0);
+    else {
+        float det = 0.0;
+        for (int i = 0; i < M.at(0).size(); i++) {
             Matrix minor;
-            copyMatrix(M,minor);
-            getMinor(minor,0,i);
-            det += pow(-1,i)*M.at(0).at(i)*determinant(minor);
+            copyMatrix(M, minor);
+            getMinor(minor, 0, i);
+            det += pow(-1, i) * M.at(0).at(i) * determinant(minor);
         }
         return det;
     }
 }
 
-void cofactors(Matrix M, Matrix &Cof){
-    zeroes(Cof,M.size());
-    for(int i=0;i<M.size();i++){
-        for(int j=0;j<M.at(0).size();j++){
+void cofactors(Matrix M, Matrix &Cof) {
+    zeroes(Cof, M.size());
+    for (int i = 0; i < M.size(); i++) {
+        for (int j = 0; j < M.at(0).size(); j++) {
             Matrix minor;
-            copyMatrix(M,minor);
-            getMinor(minor,i,j);
-            Cof.at(i).at(j) = pow(-1,i+j)*determinant(minor);
+            copyMatrix(M, minor);
+            getMinor(minor, i, j);
+            Cof.at(i).at(j) = pow(-1, i + j) * determinant(minor);
         }
     }
 }
 
-void transpose(Matrix M, Matrix &T){
-    zeroes(T,M.size());
-    for(int i=0;i<M.size();i++)
-        for(int j=0;j<M.at(0).size();j++)
+void transpose(Matrix M, Matrix &T) {
+    zeroes(T, M.size());
+    for (int i = 0; i < M.size(); i++)
+        for (int j = 0; j < M.at(0).size(); j++)
             T.at(j).at(i) = M.at(i).at(j);
 }
 
+/*
 void inversa(Matrix A, float det, Matrix &R){
   zeroes(R, A.size());
   for(int i=0;i<A.size();i++)
         for(int j=0;j<A.at(0).size();j++)
             R.at(i).at(j) = A.at(i).at(j)/det;          
 }
+ */
+
+void productRealMatrix(float real, Matrix M, Matrix &R) {
+    zeroes(R, M.size());
+    for (int i = 0; i < M.size(); i++)
+        for (int j = 0; j < M.at(0).size(); j++)
+            R.at(i).at(j) = real * M.at(i).at(j);
+}
+
+void inversa(Matrix A, Matrix &R) {
+
+    Matrix cof_matrix, adj_matrix;
+
+    float det = determinant(A);
+    float div_det = 1 /det;
+
+    if (det != 0) {
+        cofactors(A, cof_matrix);
+        transpose(cof_matrix, adj_matrix);
+
+        productRealMatrix(div_det, adj_matrix, R);
+    } else {
+        exit(EXIT_FAILURE);
+    }
+}
+
 
 #endif	/* MATH_TOOLS_H */
 
